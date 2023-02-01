12738fc5faeedefa586d4cc28560d0e3c94af154
DAdrian26 <dj_ady4you@yahoo.com>
2023-02-01 18:20:54 +0200

    Test commit

--- aws_ec2_multiple/main.tf
+++ aws_ec2_multiple/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ec2" {
 ami  =      var.ec2_ami
 instance_type =  var.instance_type
 
-#count = var.ec2_count
+count = var.ec2_count
 vpc_security_group_ids = [aws_security_group.ec2-sg.id]
 subnet_id = aws_subnet.subnet1.id
 
