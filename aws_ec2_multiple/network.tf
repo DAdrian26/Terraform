579521f23d767177ce6f05886c5e8de9ae165c04
DAdrian26 <dj_ady4you@yahoo.com>
2023-02-01 17:33:07 +0200

    Ading new resources

--- aws_ec2_multiple/network.tf
+++ aws_ec2_multiple/network.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
 # NETWORKING #
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
@@ -38,23 +32,3 @@ resource "aws_route_table_association" "rta-subnet1" {
   subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.rtb.id
 }
-
-#### INSTANCES
-
-resource "aws_instance" "ec2" {
-
-ami  =      var.ec2_ami
-instance_type =  var.instance_type
-
-#count = var.ec2_count
-vpc_security_group_ids = [aws_security_group.ec2-sg.id]
-subnet_id = aws_subnet.subnet1.id
-
-tags = {
-    
-    #Name = "${var.team}.${var.department}-${count.index}"
-    Name = "${var.team}.${var.department}"
-}
-
-}
-
